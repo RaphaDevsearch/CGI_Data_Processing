3e124143758fa97226dce9d289ab185ff172b42c
RaphaDevsearch <mizaraphaelpro@gmail.com>
2026-07-19 23:21:17 +0530

    Implement product data handling and UI updates for data management

--- cgi/utils.c
+++ cgi/utils.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "utils.h"
 
 void html_start(const char *title)
@@ -25,4 +26,84 @@ void html_end(void)
 {
   printf("</body>");
   printf("</html>");
+}
+
+void display_array(Product products[], int count)
+{
+  printf("<table border=\"1\" cellspacing=\"0\" cellpadding=\"10\">");
+
+  printf("<thead>");
+  printf("<tr>");
+  printf("<th>ID</th>");
+  printf("<th>Name</th>");
+  printf("<th>Category</th>");
+  printf("<th>Brand</th>");
+  printf("<th>Price ($)</th>");
+  printf("<th>Stock</th>");
+  printf("</tr>");
+  printf("</thead>");
+
+  printf("<tbody>");
+
+    for (int i = 0; i < count; i++)
+    {
+      printf("<tr>");
+
+      printf("<td>%d</td>", products[i].id);
+      printf("<td>%s</td>", products[i].name);
+      printf("<td>%s</td>", products[i].category);
+      printf("<td>%s</td>", products[i].brand);
+      printf("<td>%.2f</td>", products[i].price);
+      printf("<td>%d</td>", products[i].stock);
+
+      printf("</tr>");
+    }
+
+  printf("</tbody>");
+
+  printf("</table>");
+}
+
+Product *read_products(const char *filename, int *count)
+{
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL)
+    {
+        *count = 0;
+        return NULL;
+    }
+
+    char line[512];
+
+    /* Skip CSV header */
+    fgets(line, sizeof(line), file);
+
+    Product *products = NULL;
+
+    *count = 0;
+
+    while (fgets(line, sizeof(line), file))
+    {
+        products = realloc(products, sizeof(Product) * (*count + 1));
+
+        Product *product = &products[*count];
+
+        sscanf(
+            line,
+            "%d,%99[^,],%49[^,],%49[^,],%lf,%d",
+            &product->id,
+            product->name,
+            product->category,
+            product->brand,
+            &product->price,
+            &product->stock
+        );
+
+        (*count)++;
+    }
+
+    fclose(file);
+
+    return products;
 }
--- cgi/utils.h
+++ cgi/utils.h
@@ -1,7 +1,24 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#define MAX_NAME      100
+#define MAX_CATEGORY   50
+#define MAX_BRAND      50
+
+typedef struct
+{
+  int id;
+  char name[MAX_NAME];
+  char category[MAX_CATEGORY];
+  char brand[MAX_BRAND];
+  double price;
+  int stock;
+
+} Product;
+
 void html_start(const char *title);
 void html_end(void);
+Product *read_products(const char *filename, int *count);
+void display_array(Product products[], int count);
 
 #endif
